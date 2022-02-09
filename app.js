b2c390a7a1777330b70eff979d9476942b544476
Anderson <MDAnderson@samaritan.org>
2022-02-09 15:00:24 -0500

    na

--- app.js
+++ app.js
@@ -2,59 +2,37 @@ let beginning = new Date("03/16/2020"),
     timeElm = document.getElementById("time"),
     dateElm = document.getElementById("date");
 
-let tick = 0;
+(function update() {
+    let now = new Date(),
+        startOfDay = new Date();
 
-function update() {
-    // update reality
-    let now = new Date();
-    let startOfDay = new Date();
     startOfDay.setHours(0, 0, 0, 0);
-    let day = Math.round((startOfDay - beginning) / (1000 * 60 * 60 * 24)) + 16;
 
-    // set elements
-    document.title = `March ${day}th`;
+    let day = Math.round((startOfDay - beginning) / (1000 * 60 * 60 * 24)) + 16,
+        minutes = now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes(),
+        hours = now.getHours() > 12 ? now.getHours() - 12 : now.getHours(),
+        ampm = now.getHours() >= 12 ? "pm" : "am";
+
+    document.title = `${day}th`;
     dateElm.innerHTML = `March ${day}th`;
+    timeElm.innerHTML = now.getSeconds() % 2 ? `${hours}:${minutes} ${ampm}` : `${hours} ${minutes} ${ampm}`;
 
-    let minutes = now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes();
-    let hours = now.getHours() > 12 ? now.getHours() - 12 : now.getHours();
-    let ampm = now.getHours() >= 12 ? "pm" : "am";
-
-    if (tick == 0) {
-        timeElm.innerHTML = `${hours}:${minutes} ${ampm}`;
-        tick++;
-    } else {
-        timeElm.innerHTML = `${hours} ${minutes} ${ampm}`;
-        tick = 0;
-    }
-}
-
-update();
-var interval = setInterval(update, 1000);
-
+    setTimeout(update, 1000);
+})();
 
 function message() {
     let newElm = document.createElement("h1");
+    newElm.className = "message";
     newElm.innerHTML = "15 days to slow the spread";
-    newElm.style.opacity = 0;
-    newElm.style.position = "absolute";
-    newElm.style.transition = "opacity 1s ease-in-out";
-    newElm.style.top = `${Math.random() * (window.innerHeight-80)}px`;
-    newElm.style.left = `${Math.random() * (window.innerWidth-400)}px`;
-    newElm.style.fontSize = "1.5em";
+    newElm.style.top = `${Math.random() * (window.innerHeight - 80)}px`;
+    newElm.style.left = `${Math.random() * (window.innerWidth - 400)}px`;
     document.body.appendChild(newElm);
 
-    // animate newElm to fade out
-    setTimeout(function() {
-        newElm.style.opacity = .2;
-        setTimeout(function() {
-            newElm.style.opacity = 0;
-            setTimeout(function() {
-                newElm.remove();
-            }, 2000);
-        }, 2000);
-    }, 10);
+    setTimeout(() => { newElm.style.opacity = 0.2 }, 20); // fade in
+    setTimeout(() => { newElm.style.opacity = 0 }, 2000); // fade out
+    setTimeout(() => { newElm.remove() }, 4000); // remove from dom
 }
 
 document.onmouseenter = message;
-document.onclick = message;
+document.onmousedown = message;
 document.ontouchstart = message;
